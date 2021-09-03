fae036a51c6745f75203f5cec99e79dc215f157d
JBR90 <blakeage77@googlmail.com>
2021-09-03 13:55:32 +0100

    Basic styling for events page

--- src/components/Event.js
+++ src/components/Event.js
@@ -2,21 +2,24 @@ import React from "react";
 
 const Event = ({ props }) => {
   return (
-    <div className="w-full bg-white my-8">
-      <div className=" sm:justify-center lg:w-4/5 m-auto px-4 py-10 grid grid-cols-2">
-        <div className="flex    ">
+    <div className="w-full bg-white ">
+      <div className=" sm:justify-center lg:w-4/5 m-auto px-4 py-10 grid grid-cols-1 md:grid-cols-2">
+        <div className="flex order-1 md:order-1 justify-center  ">
           <img
-            className="object-cover   overflow-hidden   w-11/12  "
+            className="object-cover   overflow-hidden w-10/12  md:w-9/12 "
             src={props.image}
-            alt=""
+            alt={props.event_name}
           />
         </div>
-        <div className="flex flex-col justify-center px-4">
-          <h1 className="  bottom-56  w-full text-black text-3xl font-semibold mb-4  lg:text-4xl">
+        <div className="flex m-auto w-10/12 lg:w-full md:order-2 flex-col justify-center ">
+          <h1 className="   bottom-56  w-full text-black text-2xl font-semibold mb-4  lg:text-3xl">
             {props.event_name}
           </h1>
-          <p className="mb-4 md:text-1xl  lg:text-2xl">{props.description}</p>
-          <p className="mb-4 md:text-1xl  lg:text-2xl">{props.Date}</p>
+          <p className="mb-4 md:text-base  lg:text-1xl">{props.description}</p>
+          <p className=" text-base font-semibold  ">{`Start date: ${props.DateStart}`}</p>
+          <p className=" text-base font-semibold  ">{`End date:   ${props.DateEnd}`}</p>
+          <p className=" text-base font-semibold  ">{`Time:   ${props.Time}`}</p>
+          <p className="mb-4 text-base font-semibold  ">{`Location:   ${props.location}`}</p>
         </div>
       </div>
     </div>
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -15,7 +15,7 @@ const Events = () => {
   };
 
   events.forEach((e) => {
-    let tempDate = dateReformater(e.Date);
+    let tempDate = dateReformater(e.DateEnd);
     tempDate < date ? pastEvents.push(e) : currentEvents.push(e);
   });
 
@@ -24,72 +24,10 @@ const Events = () => {
   return (
     <div>
       {" "}
-      <div className="   z-30  w-screen h-screen  ">
-        {/* ----------------------------------------------------------------------------------- // hero // */}
-
-        <div className=" relative  w-full   h-2/4 ">
-          <div
-            className="object-cover object-right  overflow-hidden   w-screen h-full"
-            src=""
-            alt=""
-          ></div>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-56  w-full  lg:w-4/5 py-4 px-4  ">
-            <p className=" whitespace-pre-wrap w-full m-auto text-center  bottom-56  w-full text-black text-4xl ">
-              {`Events`}
-            </p>
-          </div>
-
-          {/* --------------------------------------------------------------------------------// Text + Image section 1 // */}
+      <div className="      w-screen h-screen  ">
+        <div className="   w-full py-20  h-2/4 ">
           {currentEvents.map((e) => (e = <Event key={e.id} props={e} />))}
-          <div className="w-full bg-white ">
-            <div className=" sm:justify-center lg:w-4/5 m-auto px-4 py-10 grid grid-cols-2">
-              <div className="flex    ">
-                <img
-                  className="object-cover   overflow-hidden   w-11/12  "
-                  src=""
-                  alt=""
-                />
-              </div>
-              <div className="flex flex-col justify-center px-4">
-                <h1 className="  bottom-56  w-full text-black text-3xl font-semibold mb-4  lg:text-4xl">
-                  Changing the world is possible. We’ve done it before.
-                </h1>
-                <p className="mb-4 md:text-1xl  lg:text-2xl">
-                  Our leadership team bring years of experience to bear on the
-                  greatest challenge of our time. We’re results driven, with a
-                  proven record of previous successes.
-                </p>
-                <button className="bg-black text-white w-32 hover:bg-opacity:20 text-grey-700 font-semibold  py-2 px-4 mt-4  rounded ">
-                  Learn More
-                </button>
-              </div>
-            </div>
-          </div>
-          {/* --------------------------------------------------------------------------------// Text + Image section 2 // */}
-          <div className="w-full bg-white my-8">
-            <div className=" sm:justify-center lg:w-4/5 m-auto  py-10 grid grid-cols-2">
-              <div className="flex flex-col justify-center px-4">
-                <h1 className="  bottom-56  w-full text-black text-3xl font-semibold mb-4  lg:text-4xl">
-                  Changing the world is possible. We’ve done it before.
-                </h1>
-                <p className="mb-4 md:text-1xl  lg:text-2xl">
-                  Our leadership team bring years of experience to bear on the
-                  greatest challenge of our time. We’re results driven, with a
-                  proven record of previous successes.
-                </p>
-                <button className="bg-black text-white w-32 hover:bg-opacity:20 text-grey-700 font-semibold  py-2 px-4 mt-4  rounded ">
-                  Learn More
-                </button>
-              </div>
-              <div className="flex  px-4  ">
-                <img
-                  className="object-cover   overflow-hidden   w-11/12  "
-                  src=""
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
+
           <Footer />
         </div>
       </div>
