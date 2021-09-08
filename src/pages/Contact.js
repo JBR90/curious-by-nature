cd21e88e22d9c2a7719234e2188839e0b57a16e0
JBR90 <blakeage77@googlmail.com>
2021-09-08 14:46:14 +0100

    fixed contact page footer overlap on mobile

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,8 +5,8 @@ import { FaFacebookF, FaInstagram } from "react-icons/fa";
 const Contact = () => {
   return (
     <div className="w-screen  h-screen border-2 border-indigo-200">
-      <div className="w-full  h-full m-auto lg:w-4/5 grid grid-col-1 md:grid-cols-2 gap-8 px-2 py-2 items-center ">
-        <div className="px-4  mt-32 md:mb-10 md:mt-0 xl:w-3/4 xl:ml-20">
+      <div className="w-full  md:h-full m-auto lg:w-4/5 grid grid-col-1 md:grid-cols-2 gap-8 px-2 py-2 items-center ">
+        <div className="px-4   mt-32 md:mb-10 md:mt-0 xl:w-3/4 xl:ml-20">
           <h1 className="text-5xl mb-5">Contact Us</h1>
           <p className="text-xl mb-5 ">
             A better future is possible. Contact us to learn more about our
@@ -21,7 +21,7 @@ const Contact = () => {
             <FaInstagram className="mr-3 text-2xl cursor-pointer" />
           </div>
         </div>
-        <div className="mb-16 md:mb-3 px-4 items-center xl:mr-20 ">
+        <div className="  mb-16 md:mb-3 px-4 items-center xl:mr-20 ">
           <form className="flex flex-col " action="">
             <div className="py-4">
               <label className="block  text-sm  mb-2" for="name">
